67fb4a2b561be4a3141235c8491133d5cc76b96d
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2025-02-16 11:35:14 +0530

    course card fixed

--- client/src/components/student/CoursesSection.jsx
+++ client/src/components/student/CoursesSection.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
+import { CourseCard } from "./CourseCard";
 
 const CoursesSection = () => {
+  const { allCourses } = useContext(AppContext);
+
   return (
-    <div className='px-8 py-16 md:px-40'>
-        <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
-        <p className='mt-3 text-sm text-gray-500 md:text-base'>Discover our top-rated courses across various categories. From coding and design to business and wellness, our courses are creafted to deliver results.</p>
+    <div className="px-8 py-16 md:px-40">
+      <h2 className="text-3xl font-medium text-gray-800">
+        Learn from the best
+      </h2>
+      <p className="mt-3 text-sm text-gray-500 md:text-base">
+        Discover our top-rated courses across various categories. From coding
+        and design to business and wellness, our courses are creafted to deliver
+        results.
+      </p>
 
-
-        <Link to={'/course-list'} onClick={()=>scrollTo(0,0)} className='px-10 py-3 text-gray-500 border rounded border-gray-500/30'>Show all Courses</Link>
+      <div>
+        {allCourses.slice(0, 4).map((course, index) => (
+          <CourseCard key={index} course={course} />
+        ))}
+      </div>
+      <Link
+        to={"/course-list"}
+        onClick={() => scrollTo(0, 0)}
+        className="px-10 py-3 text-gray-500 border rounded border-gray-500/30"
+      >
+        Show all Courses
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default CoursesSection
+export default CoursesSection;
